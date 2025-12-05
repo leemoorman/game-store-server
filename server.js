6f219aab5faa3c1c001f54108bef26527b078956
leemoorman <Leethalweapon487@gmail.com>
2025-12-04 23:08:43 -0500

    connecting to mongodb

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const multer = require("multer");
 const Joi = require("joi")
+const mongoose = require("mongoose");
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cors());
@@ -15,6 +16,24 @@ const storage = multer.diskStorage({
         cb(null, file.originalname);
     }
 });
+
+//testdb is name of database, it will automatically make it
+mongoose
+  .connect("mongodb+srv://leemoorman3_db_user:leemoorman3_db_user@cluster0.cfmxjey.mongodb.net/?appName=Cluster0")
+  .then(() => console.log("Connected to mongodb..."))
+  .catch((err) => console.error("could not connect ot mongodb...", err));
+
+const consoleSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  stock: Number,
+  description: String,
+  rating: Number,
+  img: String,
+});
+
+const gameConsole = mongoose.model("Console", consoleSchema);
+
 
 const upload = multer ({storage: storage});
 
@@ -93,16 +112,17 @@ const upload = multer ({storage: storage});
         }
     ];
 
-app.get("/api/consoles", (req, res) =>{
+app.get("/api/consoles", async(req, res) =>{
+    const consoles = await gameConsole.find();
     res.send(consoles);
 });
 
-app.get("/api/consoles/:id", (req, res) =>{
-    const game_console = consoles.find((game_console) => game_console._id === parseInt(req.params.id));
+app.get("/api/consoles/:id", async(req, res) =>{
+    const game_console = await gameConsole.findOne({_id: id});
     res.send(game_console);
 });
 
-app.post("/api/consoles", upload.single("img"), (req, res) =>{
+app.post("/api/consoles", upload.single("img"), async(req, res) =>{
     console.log("in post request");
     const result = validateConsole(req.body);
 
@@ -112,27 +132,26 @@ app.post("/api/consoles", upload.single("img"), (req, res) =>{
         return;
     }
 
-    const game_console = {
-        _id: consoles.length + 1,
+    const game_console = new gameConsole({
         name: req.body.name,
         price: req.body.price,
         stock: req.body.stock,
         description: req.body.description,
         rating: req.body.rating,
-    }
+    });
 
     if(req.file){
         game_console.img = req.file.filename;
     }
 
-    consoles.push(game_console);
-    res.status(200).send(game_console);
+    const newConsole = await gameConsole.save();
+    res.status(200).send(newConsole);
 });
 
-app.put("/api/consoles/:id", upload.single("img"), (req, res) =>{
-    let game_console = consoles.find((c) => c._id === parseInt(req.params.id));
+app.put("/api/consoles/:id", upload.single("img"), async(req, res) =>{
+    //let game_console = consoles.find((c) => c._id === parseInt(req.params.id));
 
-    if (!game_console) res.status(404).send("No Console with that ID");
+    //if (!game_console) res.status(404).send("No Console with that ID");
 
     const result = validateConsole(req.body);
 
@@ -142,30 +161,35 @@ app.put("/api/consoles/:id", upload.single("img"), (req, res) =>{
         return;
     }
 
-    game_console.name = req.body.name;
-    game_console.price = req.body.price;
-    game_console.stock = req.body.stock;
-    game_console.description = req.body.description;
-    game_console.rating = req.body.rating;
+    let toUpdate = {
+        name: req.body.name,
+        price: req.body.price,
+        stock: req.body.stock,
+        description: req.body.description,
+        rating: req.body.rating,
+    };
 
     if(req.file){
-        game_console.img = req.file.filename;
+        toUpdate.img = req.file.filename;
     }
 
-    res.status(200).send(game_console);
+    const updateThrough = await gameConsole.updateOne(
+        { _id: req.params.id },
+        toUpdate
+    );
+
+    const updatedConsole = await gameConsole.findOne({_id: req.params.id});
+    res.send(updatedConsole);
 });
 
-app.delete("/api/consoles/:id", (req, res) =>{
-    const game_console = consoles.find((c) => c._id === parseInt(req.params.id));
-
-    console.log(game_console);
+app.delete("/api/consoles/:id", async(req, res) =>{
+    const game_console = await gameConsole.findByIdAndDelete(req.params.id);
     
     if(!game_console){
         res.status(404).send('The console with the given id was not found');
+        return;
     }
-    
-    const index = consoles.indexOf(game_console);
-    consoles.splice(index, 1);
+
     res.status(200).send(game_console);
 });
 
